91b81e7f865f900e21cbd1a5c518d64c74c66375
DraconShade <daividqh1995@gmail.com>
2026-03-13 23:48:47 +0100

    feat: add auth routes and register auth module in app

--- src/app.js
+++ src/app.js
@@ -5,8 +5,10 @@ app.use(express.json());
 
 const mainRouter = require("./routes/main.routes.js");
 const healthRouter = require("./routes/health.routes.js");
+const authRouter = require("./routes/auth.routes.js");
 
 app.use(mainRouter);
 app.use(healthRouter);
+app.use("/api/auth", authRouter);
 
 module.exports = app;
